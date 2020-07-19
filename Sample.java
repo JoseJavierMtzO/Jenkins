adc9681a1d56379bb6464c7e2493609cd49afed3
José Javier Martínez Orozco <javiermartinez.3593@gmail.com>
2020-07-18 22:31:54 -0500

    commit

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Deploy automatico en tomcat: intento 3");
+		System.out.println("Deploy automatico en tomcat: intento 4");
 	}
 
 }
